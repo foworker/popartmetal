1946a18ca3e67501347db525f16a1223152c6278
dukkanjs <turgay@aksu.net>
2022-03-03 22:09:39 +0300

    main

--- src/pages/references.jsx
+++ src/pages/references.jsx
@@ -64,8 +64,8 @@ export default function References() {
 				{products.map((product) => (
 					<div key={product.id} className="group relative mb-6">
 						<div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-							<img
-								src={product.imageSrc.src}
+							<Image
+								src={product.imageSrc}
 								alt={product.imageAlt}
 								className="w-full h-full object-center object-cover lg:w-full lg:h-full"
 							/>
